a295592a8ce210dbcb291176927fdc645b4fd9c5
Felix Gallagher <felix.gallagher@vivideconomics.com>
2023-06-15 18:21:54 +0100

    check_na func

--- numbats/data-clean.py
+++ numbats/data-clean.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import numpy as np
 
-#numbats = pd.read_csv("")
+numbats = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-07/numbats.csv")
--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
 # util functions for data cleaning/manipulation
+
+def check_na(cols, input_df):
+    """
+    check columns of a dataframe to ensure no NA values
+    Args:
+        cols: list of column names
+        input_df: pandas dataframe
+    """
+    for col in cols:
+        if len(input_df[input_df[col].isna()]) > 0:
+            raise ValueError(f"{col} contains NA values")
