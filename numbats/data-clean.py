d33d6c5752ff3c117711e867b44b48bd8394fedf
fgallagher27 <felixgallagher@live.com>
2023-06-16 10:27:23 +0100

    premier_league brainstorm

--- numbats/data-clean.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import numpy as np
-
-numbats = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-07/numbats.csv")
---
+++ premier_league/data-clean.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+premier_leauge = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-04-04/soccer21-22.csv")
+
+print(premier_leauge.head())
+
+# categorisiation problem - label as Win, Not Win
+
+# may need log transformation
+
+# either linear probability or logistic regression
+
+# any kind of classificaton
+# random forest - classification alogrith
+# Naive bayes classification
+# Neural network 
+
+# feature importance procedure
+# ridge regression
+
